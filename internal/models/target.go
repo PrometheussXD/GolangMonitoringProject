fa10109eb7dcb903292aae9d1d0b87473eff270d
Amirreza Radfar <sarradfar@gmail.com>
2026-06-01 02:10:48 +0330

    validation added (not completely)

--- internal/models/target.go
+++ internal/models/target.go
@@ -2,7 +2,7 @@ package models
 
 type Target struct {
 	ID       uint   `gorm:"primaryKey" json:"id"`
-	Name     string `json:"name"`
-	URL      string `json:"url"`
-	Interval int    `json:"interval"`
+	Name     string `json:"name" binding:"required"`
+	URL      string `json:"url" binding:"required,url"`
+	Interval int    `json:"interval" binding:"required,min=1"`
 }
