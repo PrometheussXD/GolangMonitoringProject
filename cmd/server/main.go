bd386bf006685eb655d7a85ff78c84885e9df935
Amirreza Radfar <sarradfar@gmail.com>
2026-06-01 12:56:10 +0330

    add timeout support to monitor checker

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,11 +4,18 @@ import (
 	"Project/internal/api"
 	_ "Project/internal/dataBase"
 	db "Project/internal/dataBase"
+	"Project/internal/monitor"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	status, responseTime, isUp :=
+		monitor.CheckURL("https://google.com")
+	fmt.Println(status)
+	fmt.Println(responseTime)
+	fmt.Println(isUp)
 	db.Connect()
 	r := gin.Default()
 	api.SetupRoutes(r)
---
+++ internal/monitor/checker.go
@@ -0,0 +1,29 @@
+package monitor
+
+import (
+	"net/http"
+	"time"
+)
+
+func CheckURL(url string) (int, int64, bool) {
+
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	start := time.Now()
+
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return 0, 0, false
+	}
+
+	defer resp.Body.Close()
+
+	responseTime := time.Since(start).Milliseconds()
+	isUp := resp.StatusCode >= 200 &&
+		resp.StatusCode < 400
+
+	return resp.StatusCode, responseTime, isUp
+}
