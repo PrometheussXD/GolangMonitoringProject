466c51bbd24723380fad8d9291c36b495a0273d5
Amirreza Radfar <sarradfar@gmail.com>
2026-06-02 22:56:17 +0330

    front modified

--- internal/models/dashboard.go
+++ internal/models/dashboard.go
@@ -3,17 +3,18 @@ package models
 import "time"
 
 type DashboardTarget struct {
-	ID uint
-
-	Name string
-
-	URL string
-
-	IsUp bool
-
+	ID           uint
+	Name         string
+	URL          string
+	IsUp         bool
 	ResponseTime int64
+	LastCheck    time.Time
+	Uptime       int
+}
 
-	LastCheck time.Time
-
-	Uptime int
+func (d DashboardTarget) FormattedLastCheck() string {
+	if d.LastCheck.IsZero() {
+		return "Never"
+	}
+	return d.LastCheck.Format("2006-01-02 15:04:05")
 }
